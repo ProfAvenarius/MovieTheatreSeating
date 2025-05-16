b98413580e2b7ce268a3c02a135845fb0b904731
DC Elliott <davecliveelliott@hotmail.com>
2025-05-15 23:39:39 -0230

    Separated out main class and added a menu. Added colours to try with seat displays.

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("|=== Theatre Seating System Menu ===|");
+            System.out.println("1. View Full Seating Chart.");
+            System.out.println("2. View Available Seats Only.");
+            System.out.println("3. View Purchased Seats Only.");
+            System.out.println("4. Reserve Seats.");
+            System.out.println("5. Cancel Seats.");
+            System.out.println("6. Exit");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    SeatingChart.seatingRows();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    System.out.println("Thank you for using the Theatre Seating System. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please select a valid option.");
+            }
+        }while(choice != 6);
+    }
+}
--- src/main/java/SeatingChart.java
+++ src/main/java/SeatingChart.java
@@ -1,22 +1,25 @@
-import java.util.Arrays;
 
-public class seatingChart {
 
+public class SeatingChart {
 
     public static void seatingRows() {
-        String[][] seats = new String[26][42];
+        String[][] seats = new String[26][30];
+        System.out.println(Colours.YELLOW);
         for (char row = 'A' ; row <= 'Z' ; row++) {
             if (row > 'A') {
+
                 System.out.println();
             }
-            for (int col = 1; col <= 42; col++) {
+            for (int col = 1; col <= 30; col++) {
                 seats[row-'A'][col-1] = row + String.valueOf(col);
+
                 System.out.print(row + String.valueOf(col) + " ");
+
             }
         }
-    }
-
-    public static void main(String[] args) {
-        seatingRows();
+        System.out.println(Colours.RESET);
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
