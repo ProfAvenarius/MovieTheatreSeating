39b46463d5899c1892751fb82e7e984de762194a
DC Elliott <davecliveelliott@hotmail.com>
2025-05-19 22:04:09 -0230

    Added remove seat method.

--- src/main/java/ReserveSeats.java
+++ src/main/java/ReserveSeats.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class ReserveSeats {
     private static ArrayList<String> reservedSeats = new ArrayList<String>();
 
+    static { //Initializer block. Wanted this to run once, run first and run automatically.
+        ArrayList<String> randomSeats = RandomSeatCreator.createRandomSeats();
+        reservedSeats.addAll(randomSeats);// Initialize seat chart with random seats
+    }
+
     public static void reserveSeats() {
         Scanner scanner = new Scanner(System.in);
         int numSeats;
@@ -17,9 +22,9 @@ public class ReserveSeats {
             System.out.println("Enter the " + (i+1) + "/" + numSeats + " seat to reserve:");
             seat = scanner.nextLine();
             reservedSeats.add(seat);
+            System.out.println("Seat " + seat + " has been reserved.");
+            System.out.println();
         }
-
-        System.out.println("Reserved seats: " + reservedSeats);
     }
 
     public static ArrayList<String> getReservedSeats() {
@@ -27,8 +32,25 @@ public class ReserveSeats {
     }
 
 
+    public static void cancelSeats() {
+        Scanner scanner = new Scanner(System.in);
+        int numCancels;
+        String seatToCancel;
 
+        System.out.println("Number of seats to cancel?");
+        numCancels = scanner.nextInt();
+        scanner.nextLine();
 
-
+        for (int i = 0; i <numCancels; i++) {
+            System.out.println("Enter the " + (i+1) + "/" + numCancels + " seat to cancel:");
+            seatToCancel = scanner.nextLine();
+            if (reservedSeats.contains(seatToCancel)) {
+                reservedSeats.remove(seatToCancel);
+                System.out.println("Seat " + seatToCancel + " has been removed.");
+            }else {
+                System.out.println("Seat " + seatToCancel + " is not currently reserved.");
+                System.out.println();
+            }
+        }
+    }
 }
-
